01bb826cd41fc97288b3a43a2a9d9705a6c08093
up1 <somkiat.p@gmail.com>
2023-02-06 14:12:26 +0700

    Basic of swifty

--- day02/day02/detail/DetailProtocol.swift
+++ day02/day02/detail/DetailProtocol.swift
@@ -1,5 +1,33 @@
 import Foundation
 
-protocol DetailProtocol: AnyObject {
+protocol DetailProtocol : AnyObject {
     func setData(data : String)
+}
+
+struct UserModel : Codable {
+    
+}
+
+typealias MyFunction = (() -> Void)?
+
+class B : DetailProtocol {
+    
+    var target : MyFunction?
+    
+    func setData(data: String) {
+    }
+    
+    func process(t : MyFunction?) {
+        self.target = t
+    }
+}
+
+class A : DetailProtocol {
+    func doSth1(){
+    }
+}
+
+extension A {
+    func setData(data: String) {
+    }
 }
